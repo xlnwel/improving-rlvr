56168d0abcad5de6e4aabe77b5836596af5994f4
xinwei <122134545@qq.com>
2025-06-13 09:21:28 +0800

    Evaluate Qwen3 ICL

--- icl/eval_qwen3_icl.sh
+++ icl/eval_qwen3_icl.sh
@@ -1,14 +1,14 @@
 set -x
 
 
-python icl/eval_qwen3_icl2.py -d 1 -i system
-python icl/eval_qwen3_icl2.py -d 1 -i user
-python icl/eval_qwen3_icl2.py -d 1 -i custom
+python icl/eval_qwen3_icl.py -d 1 -i system -dt topk
+python icl/eval_qwen3_icl.py -d 1 -i user -dt topk
+python icl/eval_qwen3_icl.py -d 1 -i custom -dt topk
 
-python icl/eval_qwen3_icl2.py -d 3 -i system
-python icl/eval_qwen3_icl2.py -d 3 -i user
-python icl/eval_qwen3_icl2.py -d 3 -i custom
+python icl/eval_qwen3_icl.py -d 3 -i system -dt topk
+python icl/eval_qwen3_icl.py -d 3 -i user -dt topk
+python icl/eval_qwen3_icl.py -d 3 -i custom -dt topk
 
-python icl/eval_qwen3_icl2.py -d 5 -i system
-python icl/eval_qwen3_icl2.py -d 5 -i user
-python icl/eval_qwen3_icl2.py -d 5 -i custom
+python icl/eval_qwen3_icl.py -d 5 -i system -dt topk
+python icl/eval_qwen3_icl.py -d 5 -i user -dt topk
+python icl/eval_qwen3_icl.py -d 5 -i custom -dt topk
